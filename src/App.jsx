7c2825611dda0281d4b4e6b112be25dc9e0f9b7d
RemcoKuipers <remslaap@live.nl>
2026-02-12 16:21:22 +0100

    Gestart met Huiswerk Opdracht 9 opdracht buttons klaar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import Button from './components/button.jsx';
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  )
+    return (
+        <div>
+            <h1>Handbags & Purses</h1>
+            <section>
+                <nav>
+                    <Button buttonText="to the collection" disabled={false}/>
+                    <Button buttonText="shop all bags" disabled={false}/>
+                    <Button buttonText="shop all bags" disabled={true}/>
+                </nav>
+            </section>
+
+        </div>
+    )
 }
 
 export default App
